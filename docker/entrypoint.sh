d6eaec9d8fa1632e662e2637ac8e3149beb8f8ce
Leonardo Lima <leonardo860920@gmail.com>
2026-08-18 01:15:36 -0300

    file changes entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,23 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "Composer Install"
-composer install
-
-echo "Config env"
-exec php -r '\"file_exists('.env') || copy('.env.example', '.env');\"'
-
-echo "Key generate"
-exec php artisan key:generate
-
-echo "[entrypoint] Running migrations..."
-exec php artisan migrate --force
-
-echo "NPM Install"
-npm install --ignore-scripts
-
-echo "NPM Build"
-npm run build
 echo "[entrypoint] Starting queue worker (scans queue) in background..."
 while true; do
     exec php artisan queue:work --queue=scans,default --tries=3 --timeout=300 --sleep=1
